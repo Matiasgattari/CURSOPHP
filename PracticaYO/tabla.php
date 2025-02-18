af3e89be4d5f09973a7a3bfe38c6d37e63572f1b
Daravhel <lea.mg90@gmail.com>
2025-02-18 12:13:51 -0300

    agrego tabla practica personal

---
+++ PracticaYO/tabla.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Animes</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #000;
+            padding: 10px;
+            text-align: center;
+        }
+        th {
+            font-size: 1.2em;
+        }
+        .dia {
+            color: Black;
+        }
+        .pokemon {
+            color: yellow;
+        }
+        .digimon {
+            color: green;
+        }
+        .beta-x {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+
+<h2 style="text-align: center;">Animes según día de la semana</h2>
+
+<table>
+    <tr>
+        <th class="dia">Día</th>
+        <th class="pokemon">POKEMON</th>
+        <th class="digimon">DIGIMON</th>
+        <th class="beta-x">BETA X</th>
+    </tr>
+    <tr>
+        <td>Lunes</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+    <tr>
+        <td>Martes</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+    <tr>
+        <td>Miércoles</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+    <tr>
+        <td>Jueves</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+    <tr>
+        <td>Viernes</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+    <tr>
+        <td>Sábado</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+    <tr>
+        <td>Domingo</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        
+    </tr>
+</table>
+
+</body>
+</html>
